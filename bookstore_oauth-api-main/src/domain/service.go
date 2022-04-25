9f3f37dc57088aa66d8650b9539e9bf9eba58cb2
Ajit Singh <ajit.singh@rsystems.com>
2022-04-25 10:29:58 -0400

    Test

--- bookstore_oauth-api-main/src/domain/service.go
+++ bookstore_oauth-api-main/src/domain/service.go
@@ -1,18 +1,7 @@
 package access_token
 
-import "github.com/ajitsinghRsystems/bookstore_user-api/utils/errors"
+import "errors"
 
 type Service interface {
 	GetByID(string) (*AccessToken, *errors.RestErr)
-
-}
-type service struct{
-
-}
-func NewService() Service{
-	return &service{}
-}
-func (S *service) GetByID(string) (*AccessToken,*errors.RestErr)
-{
-	return nil,nil
 }
